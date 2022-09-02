e4ce8ab8dd296defcefec8d1cf2deabc53045384
U-REDMOND\deraskov <denplusplus@yandex-team.ru>
2022-09-02 13:53:53 -0700

    add downloaded problems

---
+++ problems/download.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import requests
+
+for i in range(1, 21):
+    url = "https://cdn.robovinci.xyz/imageframes/%d.png" % i
+    filename = "%d.png" % i
+    print("downloading: " + url)
+    resp = requests.get(url)
+    with open(filename, 'wb') as localFile:
+        localFile.write(resp.content)
