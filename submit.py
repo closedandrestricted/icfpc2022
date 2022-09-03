79e4e15c6d4964de0436930c93a8afcafa33fbf6
U-REDMOND\deraskov <denplusplus@yandex-team.ru>
2022-09-03 13:58:51 -0700

    store submitter status

--- submit.py
+++ submit.py
@@ -2,10 +2,16 @@
 
 import requests
 import os
+import json
+import hashlib
 
 NPROBLEMS = 31
+STATUS_FILENAME = "submitterStatus.json"
 
 def main():
+    with open(STATUS_FILENAME, "rb") as f:
+        status = json.load(f)
+
     auth = open("apiKey2", "rb").read().decode('utf-8').strip()
     print(auth)
     for i in range(1, NPROBLEMS):
@@ -13,8 +19,18 @@ def main():
         if not os.path.exists(filename):
             filename = "solutions/best/empty.txt"
         with open(filename, "rb") as f:
+            filedigest = hashlib.md5(open(filename, "rb").read()).hexdigest()
             resp = requests.post("https://robovinci.xyz/api/problems/%d" % i, files=[('file', f)], headers={"Authorization": "Bearer %s" % auth})
             print(resp.text)
+            taskStatus = resp.json()
+            key = "task%d" % i
+            if key not in status:
+                status[key] = {}
+            status[key]["submission_id"] = taskStatus["submission_id"]
+            status[key]["submission_digest"] = filedigest
+
+    with open(STATUS_FILENAME, "w") as f:
+        f.write(json.dumps(status, indent=4))
 
 
 if __name__ == "__main__":
