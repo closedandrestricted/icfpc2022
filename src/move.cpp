45510ab86ccede438797c24bf3a28d7c18d5f27e
Loks- <alexeypoyarkov@gmail.com>
2022-09-03 10:18:47 -0700

    Fix Decode.

--- src/move.cpp
+++ src/move.cpp
@@ -48,11 +48,11 @@ bool Move::Decode(const std::string& s) {
   if (vs[0] == "cut") {
     assert(vs.size() == 4);
     block_id1 = vs[1];
-    if (vs[2] == "x") {
+    if ((vs[2] == "x") || (vs[2] == "X")) {
       type = LINE_CUT;
       x = stoi(vs[3]);
       y = 0;
-    } else if (vs[2] == "y") {
+    } else if ((vs[2] == "y") || (vs[2] == "Y")) {
       type = LINE_CUT;
       x = 0;
       y = stoi(vs[3]);
