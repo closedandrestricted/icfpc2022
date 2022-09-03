d423be5176e2c089fd70db32e28a3dc72b3b7bbd
Loks- <alexeypoyarkov@gmail.com>
2022-09-03 10:50:01 -0700

    Update download scrupt and get new problems.

--- problems/download.py
+++ problems/download.py
@@ -2,9 +2,17 @@
 
 import requests
 
-for i in range(1, 26):
+for i in range(1, 31):
     url = "https://cdn.robovinci.xyz/imageframes/%d.png" % i
     filename = "%d.png" % i
+    print("downloading: " + url)
+    resp = requests.get(url)
+    with open(filename, 'wb') as localFile:
+        localFile.write(resp.content)
+
+for i in range(26, 31):
+    url = "https://cdn.robovinci.xyz/imageframes/%d.initial.json" % i
+    filename = "%d.initial.json" % i
     print("downloading: " + url)
     resp = requests.get(url)
     with open(filename, 'wb') as localFile:
