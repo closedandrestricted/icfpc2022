c34acb4517849101180883961c0f1ec38a1bd230
Loks- <alexeypoyarkov@gmail.com>
2022-09-03 14:12:45 -0700

    Enable GreedySplit for latest problems.

--- src/solvers/greedy_split.h
+++ src/solvers/greedy_split.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include "canvas.h"
+#include "image.h"
 #include "problem.h"
 #include "solution.h"
 #include "solvers/base.h"
 
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace src_solvers {
 class GreedySplit : public Base {
@@ -19,9 +22,11 @@ class GreedySplit : public Base {
     return std::make_shared<GreedySplit>(*this);
   }
 
-  // bool SkipSolutionRead() const override { return true; }
+  bool SkipSolutionRead() const override { return true; }
 
   std::string Name() const override { return "greedy_split"; }
+
+  std::vector<Move> SolveI(const Image& target, const Canvas& canvas);
 
   Solution Solve(const Problem& p) override;
 };
