8711534f12af661a2e2ed706e179026197876a0f
U-REDMOND\deraskov <denplusplus@yandex-team.ru>
2022-09-03 12:57:46 -0700

    submit script

---
+++ submit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+NPROBLEMS = 31
+
+def main():
+    auth = open("apiKey2", "rb").read().decode('utf-8').strip()
+    print(auth)
+    for i in range(1, NPROBLEMS):
+        filename = "solutions/best/%d.txt" % i
+        if os.path.exists(filename):
+            with open(filename, "rb") as f:
+                resp = requests.post("https://robovinci.xyz/api/problems/%d" % i, files=[('file', f)], headers={"Authorization": "Bearer %s" % auth})
+                print(resp.text)
+
+
+if __name__ == "__main__":
+    main()
