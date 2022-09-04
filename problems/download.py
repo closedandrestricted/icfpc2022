a66d9aad2af2e38e342b9e9305ccbb951eaa607c
Loks- <alexeypoyarkov@gmail.com>
2022-09-04 08:51:11 -0700

    Download new problems.

--- problems/download.py
+++ problems/download.py
@@ -12,9 +12,33 @@ for i in range(1, NPROBLEMS):
     with open(filename, 'wb') as localFile:
         localFile.write(resp.content)
 
+for i in range(1, NPROBLEMS):
+    url = "https://cdn.robovinci.xyz/imageframes/%d.json" % i
+    filename = "%d.json" % i
+    print("downloading: " + url)
+    resp = requests.get(url)
+    with open(filename, 'wb') as localFile:
+        localFile.write(resp.content)
+
 for i in range(26, NPROBLEMS):
     url = "https://cdn.robovinci.xyz/imageframes/%d.initial.json" % i
     filename = "%d.initial.json" % i
+    print("downloading: " + url)
+    resp = requests.get(url)
+    with open(filename, 'wb') as localFile:
+        localFile.write(resp.content)
+
+for i in range(36, NPROBLEMS):
+    url = "https://cdn.robovinci.xyz/sourcepngs/%d.source.png" % i
+    filename = "%d.source.png" % i
+    print("downloading: " + url)
+    resp = requests.get(url)
+    with open(filename, 'wb') as localFile:
+        localFile.write(resp.content)
+
+for i in range(36, NPROBLEMS):
+    url = "https://cdn.robovinci.xyz/sourcepngs/%d.source.json" % i
+    filename = "%d.source.json" % i
     print("downloading: " + url)
     resp = requests.get(url)
     with open(filename, 'wb') as localFile:
