c96307ba0c5630b606d0153667d1c491ea126f3e
Den Raskovalov <denbox@gmail.com>
2022-09-03 23:49:16 -0700

    add dim method

--- src/block.h
+++ src/block.h
@@ -11,7 +11,9 @@ class Block {
   double Size() const { return (x1 - x0) * (y1 - y0); }
   unsigned Width() const { return x1 - x0; };
   unsigned Height() const { return y1 - y0; };
-  std::pair<unsigned, unsigned> Dim() const {
+
+  using TDim = std::pair<unsigned, unsigned>;
+  TDim Dim() const {
     return std::make_pair(Width(), Height());
   }
 };
