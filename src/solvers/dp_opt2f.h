1861333b4bca307faad326bd5fecbab1a948cf24
Loks- <alexeypoyarkov@gmail.com>
2022-09-04 23:58:48 -0700

    Add flipped version for DP2.

--- src/solvers/dp_opt2f.h
+++ src/solvers/dp_opt2f.h
@@ -9,23 +9,19 @@
 #include <vector>
 
 namespace src_solvers {
-class DPOpt2 : public Base {
+class DPOpt2F : public Base {
  public:
   using TBase = Base;
   unsigned max_xy;
 
  public:
-  static std::vector<Move> SolveIY(const Image& target, const Canvas& canvas,
-                                   unsigned nx, unsigned ny);
+  DPOpt2F(unsigned _max_xy) : max_xy(_max_xy) {}
 
- public:
-  DPOpt2(unsigned _max_xy) : max_xy(_max_xy) {}
-
-  PSolver Clone() const override { return std::make_shared<DPOpt2>(*this); }
+  PSolver Clone() const override { return std::make_shared<DPOpt2F>(*this); }
 
   bool SkipSolutionRead() const override { return true; }
 
-  std::string Name() const override { return "dp2_" + std::to_string(max_xy); }
+  std::string Name() const override { return "dp2f_" + std::to_string(max_xy); }
 
   std::vector<Move> SolveI(const Image& target, const Canvas& canvas);
 
--- src/utils/flip.h
+++ src/utils/flip.h
@@ -2,6 +2,8 @@
 
 #include "block.h"
 #include "image.h"
+#include "move.h"
 
 Block FlipXY(const Block& b);
 Image FlipXY(const Image& image);
+Move FlipXY(const Move& m);
